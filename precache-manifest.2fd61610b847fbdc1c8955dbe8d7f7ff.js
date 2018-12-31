446bf9f15aacb1ae92d38c532e2be656689eb9b9
sapdop <vatsan.pb@outlook.com>
2018-12-31 15:06:43 +0530

    Updates

---
+++ precache-manifest.2fd61610b847fbdc1c8955dbe8d7f7ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad0ffb6ad80e8ea22183",
+    "url": "/powergrid-level-editor/static/js/runtime~main.ad0ffb6a.js"
+  },
+  {
+    "revision": "54461033bc32df38b961",
+    "url": "/powergrid-level-editor/static/js/main.54461033.chunk.js"
+  },
+  {
+    "revision": "77ceb03d43ef3f76421e",
+    "url": "/powergrid-level-editor/static/js/1.77ceb03d.chunk.js"
+  },
+  {
+    "revision": "54461033bc32df38b961",
+    "url": "/powergrid-level-editor/static/css/main.0e0929bc.chunk.css"
+  },
+  {
+    "revision": "5c5cb44b7a8d353a1d3597c5711376c6",
+    "url": "/powergrid-level-editor/index.html"
+  }
+];
